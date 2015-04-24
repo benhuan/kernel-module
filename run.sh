5609a98265ccfd662f1a048cd98ec301a180dd5a
lqzhang <lqzhang@ntu.edu.sg>
2015-04-24 17:10:22 +0800

    update script

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-sudo rmmod hello-5
-sudo insmod hello-5.ko mylong=123987 mystring="zhanglongqi--++\\\\" myshort=251
+sudo rmmod $1
+sudo insmod $1.ko mylong=123987 mystring="zhanglongqi--++\\\\" myshort=251
 tail -n 20 /var/log/kern.log
-sudo rmmod hello-5
+sudo rmmod $1
