af165fbb82efade6f3c8795c22a6c0a93e8ad54b
lqzhang <lqzhang@ntu.edu.sg>
2015-04-24 17:15:08 +0800

    update script, using parameter

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
+if [ "$#" -ne 1 ]; then
+    echo "Illegal number of parameters"
+    exit 0
+fi
 sudo rmmod $1
 sudo insmod $1.ko mylong=123987 mystring="zhanglongqi--++\\\\" myshort=251
 tail -n 20 /var/log/kern.log
